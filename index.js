1d2a90ade6020bc4ce27b5419e6cf09c0b275106
Suma Kartik <pair+kartiksuma@forcenex.us>
2022-03-04 13:03:08 -0500

    added cookie parser

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const cookieParser = require("cookie-parser");
+
+const app = express();
+const port = 3000;
+
+let name = "";
+
+app.get("/login/:name", (req, res) => {
+  const opts = {
+    httpOnly: true,
+    secure: false,
+  };
+  name = req.params.name;
+  console.log(`User ${name} attempting login.`);
+  res.cookie("loginCookie", { userName: name }, opts);
+  res.end();
+});
+
+app.use(cookieParser());
+
+app.get("/hello", (req, res) => {
+  const cookieObj = req.cookies.loginCookie;
+  // console.table(cookieObj.userName);
+  res.status(200);
+  res.send(`Hello ${cookieObj.userName}`);
+  // console.log(cookieObj.userName);
+});
+
+app.listen(port, () => console.log("Server running"));
